3a06926b943d8cc1bdbd8825025e80eff1d26c96
Ranjit S <ranjitshanker.s@gmail.com>
2022-05-31 16:09:18 +1000

    initial version of robot.py
    Coding challenge toy robot - main file is robot.py

---
+++ robot.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+##############################################################################
+# robot.py
+# Toy robot coding challenge
+# Date of creation: 31 May 2022
+# Author: Ranjit
+##############################################################################
+"""
+Main file that accepts command line arguments and call the function to parse them
+"""
+import sys,getopt
+from command import parse_commands
+
+def usage():
+    print("Usage: "+sys.argv[0]+ "-f <file_name> \nOR\nUsage: "+\
+    sys.argv[0]+ "-c <commands>")    
+    
+def main():
+    argv = sys.argv[1:]   
+    file_commands = ""
+    command_list = ""
+    
+    try:
+	    opts, args = getopt.getopt(argv, "f:c:")
+        if not opts:
+            print("No options supplied")
+            usage()
+    except getopt.GetoptError as err:
+        # print help information and exit 
+        # ref: #https://docs.python.org/3/library/getopt.html
+        print(err)
+        usage()
+        # Unix programs use 2 for command line syntax errors 
+        # and 1 for all other kind of errors 
+        # ref: https://docs.python.org/3/library/sys.html
+        sys.exit(2) 
+
+	for opt, arg in opts:
+		if opt in ['-f']:
+			file_name = arg
+            # open the file in read mode
+            #  good practice to use the with keyword when dealing with file objects. 
+            # ADV: file is properly closed, even if an exception is raised. 
+            # Using with is also much shorter than writing equivalent try-finally blocks:
+            # ref: https://docs.python.org/3/tutorial/inputoutput.html
+            with open(file_name, 'r', encoding="utf-8") as file:
+                # read all lines at once
+                command_list = file.read()
+		elif opt in ['-c']:
+			command_list = arg            
+        else:
+            #ref: https://docs.python.org/3/library/getopt.html
+            print("Unhandled option" )
+            usage()
+            sys.exit(2) 
+ 
+    # parse the list of commands
+    parse_commands(command_list)
+        
+if __name__ == '__main__':
+    main()
