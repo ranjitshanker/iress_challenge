376a2a8e53059cc0cba06c46fbf9e17607fe9d52
Ranjit S <ranjitshanker.s@gmail.com>
2022-06-01 09:36:36 +1000

    initial

---
+++ movement.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+##############################################################################
+# movement.py
+# Toy robot coding challenge
+# Date of creation: 31 May 2022
+# Author: Ranjit
+##############################################################################
+"""
+Move the robot subject to the following constraints
+1. Move the toy robot one unit forward in the direction it is currently facing.
+2. Any movement that would result in the robot falling from the table must be prevented,
+however further valid movement commands must still be allowed.
+"""
