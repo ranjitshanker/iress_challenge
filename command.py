883730086b2f73d7056be7cd768e4e8d5560bdbd
Ranjit S <ranjitshanker.s@gmail.com>
2022-05-31 16:48:00 +1000

    initial command.py

---
+++ command.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+##############################################################################
+# command.py
+# Toy robot coding challenge
+# Date of creation: 31 May 2022
+# Author: Ranjit
+##############################################################################
+"""
+Parse the  list of commands
+"""
+#from movement import Robot
+
+def parse_commands(command_list):
+    print(command_list)
+
+#robot = Robot()
+
+#robot.move()
