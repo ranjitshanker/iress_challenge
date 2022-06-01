7f263422cc1dd6ae1024d926b21b47c521356cc4
Ranjit S <ranjitshanker.s@gmail.com>
2022-06-01 18:17:17 +1000

    changed the user input logic

--- robot.py
+++ robot.py
@@ -10,49 +10,39 @@
 Main file that accepts command line arguments
 1. Input can be from a file, or from standard input.
 2. Application should handle error states appropriately and be robust to user input
+3. Create a console application that can read in commands of the following form -
+     PLACE X,Y,F
+     MOVE
+     LEFT
+     RIGHT
+     REPORT
 """
-import sys,getopt
+import sys
 from command import parse_commands
-
-def usage():
-    print("Usage: "+sys.argv[0]+ " -f <file_name> \nOR\nUsage: "+\
-    sys.argv[0]+ " -c '<commands>'")    
     
 def main():
-    argv = sys.argv[1:]   
-    file_commands = ""
+    len_arg = len(sys.argv)
     command_list = ""
-    
-    try:
-        opts, args = getopt.getopt(argv, "f:c:")
-        if not opts:
-            print("ERROR!! No options supplied")
-            usage()
-    except getopt.GetoptError as err:
-        # print help information and exit 
-        # ref: #https://docs.python.org/3/library/getopt.html
-        print("ERROR!! ",err)
-        usage()
-        # Unix programs use exit(2) for command line syntax errors 
-        # and 1 for all other kind of errors 
-        # ref: https://docs.python.org/3/library/sys.html
-        sys.exit(2) 
-
-    for opt, arg in opts:
-        if opt in ['-f']:
-            file_name = arg
-            try:
-                # open the file in read mode:
-                # ref: https://docs.python.org/3/tutorial/inputoutput.html
-                with open(file_name, 'r', encoding="utf-8") as file:
-                    # read all lines at once
-                    command_list = file.read()
-            except IOError:
+ 
+    if len_arg > 1  :
+        file_name = sys.argv[1]  
+        try:
+            with open(file_name, 'r', encoding="utf-8") as file:
+                # read all lines at once
+                command_list = file.read()
+        except IOError:
                 print("ERROR!!Check the file")
                 sys.exit(2) 
-        
-        elif opt in ['-c']:
-            command_list = arg
+    else:
+        print("Enter commands line by line , type exit to quit")
+        while True:          
+            # without strip(), if you type "quit", line will actually be "quit\n", 
+            # because readlines does not strip newlines.
+            line = sys.stdin.readline().rstrip('\n')
+            if line.lower() == 'exit':
+                break
+            else:
+                command_list = '\n'.join([command_list,line])
  
     # parse the list of commands
     parse_commands(command_list)
