ac1fbb46d8991333005a9c0a6dbd1e0025e79494
Ranjit S <ranjitshanker.s@gmail.com>
2022-06-01 12:30:42 +1000

    Init method of Robot class

--- movement.py
+++ movement.py
@@ -12,3 +12,13 @@ Move the robot subject to the following constraints
 2. Any movement that would result in the robot falling from the table must be prevented,
 however further valid movement commands must still be allowed.
 """
+class Robot():
+    def __init__(self,full_command,x,y,position,exec_command):
+        self.full_command = full_command
+        self.x = x
+        self.y = y
+        self.position = position
+        self.exec_command = exec_command
+    
+    def print_cmd(self):
+        print("INPUT=",self.full_command,". X=",self.x,". Y=",self.y,". Position=",self.position,". Execute=",self.exec_command)
