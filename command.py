2cdd0856d34ede871959b0bf2c4ecc47a006a609
Ranjit S <ranjitshanker.s@gmail.com>
2022-06-01 12:30:02 +1000

    pass arguments to Robot class

--- command.py
+++ command.py
@@ -19,7 +19,7 @@ except ImportError:
 finally:
     import configparser
     
-#from movement import Robot
+from movement import Robot
 
 # To prevent ParsingError, set allow_no_value=True
 # ref: https://stackoverflow.com/questions/51864289/configparser-parsingerror-source-contains-parsing-errors-my-ini
@@ -41,23 +41,28 @@ def check_command(command):
     sub_cmd_list = [x for x in sub_cmd_list if x]
     # Finally we have a command set as shown below
     # eg: [' 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE']
-    # The first command is ALWAYS of the form X,Y, Position..
-    # if 0<=x<=5 and 0<=y<=5 and position in (N,S,E,W) then proceed.
-    check_place = sub_cmd_list[0].split(",")
+    # The first command is ALWAYS of the form X,Y, Position.
+    # Location must satisfy 0<=x<=5 and 0<=y<=5 and position must be (N,S,E,W)
+    # The rest of the commands, must be MOVE or LEFT or RIGHT
+    check_place = sub_cmd_list[0].split(",") # split the place command into X,Y, Position
     if config['Constraints']['Xmin'] <= check_place[0] <= config['Constraints']['Xmax'] and \
     config['Constraints']['Ymin'] <= check_place[1] <= config['Constraints']['Ymax']  and \
-    check_place[2].lower() in config.options('Position'):
+    check_place[2].lower() in config.options('Position') and \
+    all(elements in config.options('Commands') for elements in [x.lower() for x in sub_cmd_list[1:]]):
         # Concat PLACE to start of command set  
         sub_cmd_list[0]=start_cmd+' '+sub_cmd_list[0]
-        # Rest of the commands must be MOVE or LEFT or RIGHT 
-        sub_cmd_list[1:] = [x for x in sub_cmd_list[1:] if x.lower() in config.options('Commands')]   
+        # To ignore any typo mistakes by user in MOVE,LEFT or RIGHT, uncomment below
+        #sub_cmd_list[1:] = [x for x in sub_cmd_list[1:] if x.lower() in config.options('Commands')]   
         # Concat REPORT to end of command set  
         sub_cmd_list.append(end_cmd) 
-        print(sub_cmd_list)
-        #robot = Robot()
-        #robot.move()
+        #print(sub_cmd_list)       
+        # Pass full_command, coordinates, and commands between PLACE & REPORT to the Robot
+        len_cmd = len(sub_cmd_list[1:]) 
+        robot = Robot(sub_cmd_list,check_place[0],check_place[1],\
+        check_place[2].lower(),[x.lower() for x in sub_cmd_list[1:len_cmd]])
+        robot.print_cmd()
     else:
-        print("Illegal command")
+        print("INPUT =",[start_cmd+command+end_cmd],". OUTPUT = Error! Illegal command")
     
 def parse_commands(command_list):
     new_command_list=[]   
